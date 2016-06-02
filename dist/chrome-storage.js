9af2480a911b5d3ef5f8be26e0d5414e2ed88969
akonwi <akonwi@gmail.com>
2016-06-01 21:30:54 -0400

    Update distribution file

--- dist/chrome-storage.js
+++ dist/chrome-storage.js
@@ -7,7 +7,7 @@ exports.create = create;
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var version = { value: '1.0.0' };
+var version = { value: '1.0.1' };
 
 var LOCAL = "local";
 exports.LOCAL = LOCAL;
